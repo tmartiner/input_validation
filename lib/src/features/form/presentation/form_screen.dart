122aa91278994e5c5b139088c7dadf06d7ce44f5
vonmartiner <martiner.thomas@yahoo.de>
2024-05-23 10:58:42 +0200

    coole aufgabe

--- lib/src/features/form/presentation/form_screen.dart
+++ lib/src/features/form/presentation/form_screen.dart
@@ -44,12 +44,24 @@ class FormScreen extends StatelessWidget {
   }
 
   String? validateEmail(String? input) {
-    // TODO: implementiere hier die Logik, die im Task Sheet beschrieben ist
+    if (input == null || input.isEmpty) {
+      return 'Email darf nicht leer sein';
+    } else if (input.length <= 5) {
+      return 'Email muss mehr als 5 Zeichen haben';
+    } else if (!input.contains('@')) {
+      return 'Email muss ein "@" Zeichen enthalten';
+    } else if (!(input.endsWith('.com') || input.endsWith('.de'))) {
+      return 'Email muss mit ".com" oder ".de" enden';
+    }
     return null;
   }
 
   String? validatePw(String? input) {
-    // TODO: implementiere hier die Logik, die im Task Sheet beschrieben ist
+    if (input == null || input.isEmpty) {
+      return 'Passwort darf nicht leer sein';
+    } else if (input.length < 6 || input.length > 12) {
+      return 'Passwort muss zwischen 6 und 12 Zeichen lang sein';
+    }
     return null;
   }
 }
